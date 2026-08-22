0a4f7b9a4daf4b25a96fc176d5b858297742fb1e
Hleb <shevardingleb10@gmail.com>
2026-08-22 22:55:16 +0300

    feat: create second level nesting markup and styles for sidebar headings

--- src/pages/products/components/sideBar/SideBar.jsx
+++ src/pages/products/components/sideBar/SideBar.jsx
@@ -64,18 +64,40 @@ export default function SideBar() {
                 {categories.map((category) => (
                     <div className={styles.categoryBlock}>
                         <div
-                            key={category.name} className={`${styles.tab} ${activeCat === category.name ? styles.active : ""}`}
+                            key={category.name}
+                            className={`${styles.tab} ${activeCat === category.name ? styles.active : ""}`}
                             onClick={() => toggle(category.name)}
                         >
                             <div className={styles.tabWrapper}>
-                                <div className={styles.tabTitle}>{category.name}</div>
-                                {category.items.length > 0 && (
+                                {category.name}
+                                {category.items?.length > 0 && (
                                     <div className={styles.arrow}>
                                         <span></span>
                                     </div>
                                 )}
                             </div>
                         </div>
+
+                        {activeCat === category.name && category.items.length > 0 && (
+                            <div className={styles.subMenu}>
+                                {category.items.map((subCat) => (
+                                    <div
+                                        key={subCat.name}
+                                        className={`${styles.tab} && ${activeSubCat === subCat.name ? styles.active : ""}`}
+                                        onClick={(e) => subToggle(e, subCat.name)}
+                                    >
+                                        <div className={styles.tabWrapper}>
+                                            {subCat.name}
+                                            {subCat.items?.length > 0 && (
+                                                <div className={styles.arrow}>
+                                                    <span></span>
+                                                </div>
+                                            )}
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
                     </div>
                 ))}
             </div>
