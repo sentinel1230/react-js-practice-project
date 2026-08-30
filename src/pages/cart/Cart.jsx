39689a8c633ce91abe90a78e7855ef90522dc04d
Hleb <shevardingleb10@gmail.com>
2026-08-30 17:16:49 +0300

    fix: rename корзина to cart

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -20,7 +20,7 @@ export default function Cart() {
 
     return (
         <div className={styles.main}>
-            <h1 className={styles.title}>Корзина</h1>
+            <h1 className={styles.title}>Cart</h1>
 
             <div className={styles.itemsList}>
                 {items.map((item) => (
